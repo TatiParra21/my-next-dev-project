22e1151fdf9958f5dac87b3ad45b9f4f04dfe029
TatiParra21 <tatiparras21@gmail.com>
2025-10-30 13:59:31 -0500

    consoles

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,19 +9,21 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') }) // ✅
 import { router } from './project_ideas_db';
 import cors from 'cors';
 import express from 'express'
-
+import listEndpoints from "express-list-endpoints";
 const app = express();
 const PORT = 3000
 // 👇 Serve your built React files
+console.log("tehe static start")
 app.use(express.static(path.join(__dirname, "out/renderer")));
-
+console.log("the /* route")
 //  Fallback to index.html for React Router
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "out/renderer/index.html"));
 });
 
+console.log("🧭 Registered routes:", listEndpoints(app));
 // Start the server
-
+console.log("server is staring")
 app.use(cors())
 app.use(express.json())
 app.use("/database",router)
@@ -36,5 +38,5 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-import listEndpoints from "express-list-endpoints";
+
 console.log("🧭 Registered routes:", listEndpoints(app));
